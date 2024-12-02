1a1c01c249dd949f9d1edec72a725c7cc85a2e11
Wong-Flo <florian.wong@outlook.com>
2024-12-02 11:55:24 +0100

    Add files to deploy on Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { createRequestHandler } from '@expo/server/adapter/vercel';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default createRequestHandler({
   build: path.join(__dirname, '../dist/server'),
 });
